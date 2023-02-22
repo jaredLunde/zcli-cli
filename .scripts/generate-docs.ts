965a0b0c4348cffc6b314d22dccec00b99ac2c6d
Jared Lunde <jared.lunde@gmail.com>
2023-02-21 22:59:20 -0500

    bump

--- .scripts/generate-docs.ts
+++ .scripts/generate-docs.ts
@@ -6,13 +6,16 @@ await zcliDoc(app, root, {
   output: "README.md",
   title: "zCLI",
   description: `
-    A command-line tool for easily creating [zCLI](https://github.com/jaredLunde/zcli) 
-    applications and commands with Deno.
-
-    Get started by running \`zcli init\` to create a new zCLI application. For
-    more information, see [\`zcli init\`](#-zcli-init).
-
+    A command-line tool for easily managing
+    [zCLI](https://github.com/jaredLunde/zcli) applications and commands with Deno.
+    
+    Get started by running \`zcli init\` to create a new zCLI application. For more
+    information, see [\`zcli init\`](#-zcli-init).
+    
     Then run \`zcli add <command>\` to add a new command to your zCLI application.
+    The command will be created in the \`commands\` directory of your zCLI application.
+    It will be automatically imported and added to the \`commands\` array in your
+    \`commands/mod.ts\` file. Sub-commands are supported.
   `,
   ignoreCommands(_cmd, path) {
     return path.length > 1 && path.includes("help");
