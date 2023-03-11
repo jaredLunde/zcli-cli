2a79031f0728c7517d7f87c001b3f9d71cebbcfe
Jared Lunde <jared.lunde@gmail.com>
2023-03-11 00:08:55 -0500

    feat: improve app structure

--- commands/mod.ts
+++ commands/mod.ts
@@ -1,3 +1,4 @@
+import { app, completion, version } from "../zcli.ts";
 import { add } from "./add/mod.ts";
 import { init } from "./init/mod.ts";
 import { config } from "./config/mod.ts";
@@ -7,3 +8,21 @@ export const commands = [
   init,
   config,
 ];
+
+export const root = app.command("zcli", {
+  short:
+    "A command-line tool for easily creating zCLI applications and commands with Deno.",
+  long: `
+    A command-line tool for easily creating zCLI applications and commands with Deno.
+
+    Get started by running \`zcli init\` to create a new zCLI application. For
+    more information, run \`zcli help init\`.
+
+    Then run \`zcli add <command>\` to add a new command to your zCLI application.
+  `,
+  commands: [
+    ...commands,
+    version(app),
+    completion(app),
+  ],
+});
